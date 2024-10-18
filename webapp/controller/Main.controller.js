3d1a7f6e99cb273819ff8212645187c700176244
Aleksandra Muhina <aleksandra.muhina@roiable.com>
2024-10-18 14:04:22 +0000

    add refresh button

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,12 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+	"sap/m/MessageBox"
 ],
-function (Controller) {
+function (Controller, MessageToast, MessageBox) {
     "use strict";
 
     return Controller.extend("tutorial.tutorial.controller.Main", {
-        onInit: function () {
+        onInit() {
 
+        },
+
+        onRefresh() {
+            const oBinding = this.byId("peopleList").getBinding("items");
+
+            //.hasPendingChanges() is a method of sap.ui.model.odata.v4.ODataPropertyBinding, 
+            //and returns boolean true or false
+            if(oBinding.hasPendingChanges()) {
+                MessageBox.error(this._getText("referenceNotPossibleMessage"));
+                return;
+            }
+            oBinding.refresh();
+            MessageToast.show(this._getText("refreshSuccessMessage"));
+        }, 
+
+        //Get messages from the i18n model
+        _getText(sTextId) {
+            return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId);
         }
     });
 });
